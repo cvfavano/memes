187f3c573af5f6a5afbe8e4739362ccaf97a6abf
Christa Favano <crstav@gmail.com>
2024-08-04 14:19:25 -0400

    Start form for meme

---
+++ src/components/Meme.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Meme() {
+    return (
+        <main>
+            <div className="container">
+        <form>
+            <input type="text" />
+            <input type="text" />
+            <button>Get a mew meme image 🖼</button>
+        </form>
+        </div>
+        </main>
+        
+    )
+
+
+}
+
+export default Meme
